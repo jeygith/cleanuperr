f905b17b3a4116e5c657023d4e087f12271b789f
Marius Nechifor <flm.marius@gmail.com>
2025-01-01 17:09:06 +0200

    Add app version logging on startup (#31)

--- code/Executable/Program.cs
+++ code/Executable/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Executable.DependencyInjection;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -6,5 +7,15 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Logging.AddLogging(builder.Configuration);
 
 var host = builder.Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+var version = Assembly.GetExecutingAssembly().GetName().Version;
+
+logger.LogInformation(
+    version is null
+        ? "cleanuperr version not detected"
+        : $"cleanuperr v{version.Major}.{version.Minor}.{version.Build}"
+);
 
 host.Run();
